4c5f1f60774114bc2949a0fa1ef07bbc300a0965
Holger Langenau <holger.langenau@mathematik.tu-chemnitz.de>
2014-02-15 09:16:38 +0100

    Basic color picking from root window.

---
+++ main.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+#include <X11/Xlib.h>
+
+int main(int argc, char **argv)
+{
+    Display *display;
+    int screen;
+    Window root;
+
+    Window rr, cr;
+    int wrx, wry;
+    unsigned int mr;
+
+    display = XOpenDisplay(0);
+    screen = DefaultScreen(display);
+    root = RootWindow(display, screen);
+
+    int ptr_x, ptr_y;
+    unsigned long color;
+
+    XQueryPointer(display, root, &rr, &cr, &ptr_x, &ptr_y, &wrx,
+            &wry, &mr);
+
+    XImage *img = XGetImage(display, root, ptr_x, ptr_y, 1, 1,
+            XAllPlanes(), ZPixmap);
+
+    if (img != NULL) {
+        printf("got image @(%d, %d)\n", ptr_x, ptr_y);
+        color = XGetPixel(img, 0, 0);
+        printf("#%08x\n", color);
+        XFree(img);
+    }
+    else {
+        printf("error getting image @(%d, %d)\n", ptr_x, ptr_y);
+    }
+
+    return 0;
+}
